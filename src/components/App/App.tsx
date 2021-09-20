4497dc857d7427739cfdc572a23bf82a4045e1c4
Talita Azevedo <talita.azevedo360@gmail.com>
2021-09-20 01:49:08 -0300

    Creating Math Funcions on Wrapper and App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,15 +2,51 @@ import React, { FunctionComponent, useState } from "react";
 import { Wrapper } from "../Wrapper";
 import Display from "../Display";
 import { StyledApp } from "./styled";
+import { Digit, Operator } from "../../lib/types";
 export const App: FunctionComponent = () => {
     // Calc States
     const [display, setDisplay] = useState<string>("0");
     const [memory, setMemory] = useState(0);
+    const [result, setResult] = useState(0);
+
+    const [waitingForOperand, setWaitingForOperand] = useState(true);
+    const [pendingOperator, setPendingOperator] = useState<Operator>();
+    // Calc Function
+    const calculate = (
+        rightOperand: number,
+        pendinOperator: Operator
+    ): boolean => {
+        let newResult = result;
+        switch (pendinOperator) {
+            case "+":
+                newResult += rightOperand;
+                break;
+            case "-":
+                newResult -= rightOperand;
+                break;
+            case "×":
+                newResult *= rightOperand;
+                break;
+            case "÷":
+                if (rightOperand === 0) {
+                    return false;
+                }
+                newResult /= rightOperand;
+        }
+        setResult(newResult);
+        setDisplay(newResult.toString().toString().slice(0, 12));
+
+        return true;
+    };
     return (
         <StyledApp>
             <Display
                 value={display}
-                expression={"expression"}
+                expression={
+                    typeof pendingOperator !== "undefined"
+                        ? `${result} ${pendingOperator} ${waitingForOperand}? '' : display `
+                        : ""
+                }
                 hasMemory={memory !== 0}
             />
             <Wrapper></Wrapper>
--- src/components/Wrapper/index.tsx
+++ src/components/Wrapper/index.tsx
@@ -1,45 +1,88 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useEffect } from "react";
 import Button from "../Button";
 import { StyledWrapper } from "./styled";
+import { Digit, Operator } from "../../lib/types";
 
-export const Wrapper: FunctionComponent = () => {
-    const handleKeyDown = (): void => {};
+interface WrapperProps {
+    onDigitButtonClick?: (digit: Digit) => void;
+    onPointButtonClick?: () => void;
+    onOperatorButtonClick?: (operator: Operator) => void;
+    onChangesSignButtonClick?: () => void;
+    onEqualButtonClick?: () => void;
+    onAllClearButtonClick?: () => void;
+    onClearEntryButtonClick?: () => void;
+    onMemoryRecallButtonClick?: () => void;
+    onMemoryClearButtonClick?: () => void;
+    onMemoryPlusButtonClick?: () => void;
+    onMemoryMinusButtonClick?: () => void;
+}
+
+export const Wrapper: FunctionComponent<WrapperProps> = ({
+    onDigitButtonClick,
+    onPointButtonClick,
+    onOperatorButtonClick,
+    onChangesSignButtonClick,
+    onEqualButtonClick,
+    onAllClearButtonClick,
+    onClearEntryButtonClick,
+    onMemoryRecallButtonClick,
+    onMemoryClearButtonClick,
+    onMemoryMinusButtonClick,
+    onMemoryPlusButtonClick,
+}) => {
+    const handleKeyDown = ({ key, shiftKey }: KeyboardEvent) => {
+        console.log(key);
+
+       switch(key){
+           case key === '0' ||'1' ||'2' || '3'|| '4'|| '5'|| '6'|| '7'|| '8'|| '9' && !shiftKey:
+               onDigitButtonClick((parseFloat(key) - 0)as Digit)
+
+
+
+        }
+
+    useEffect(() => {
+        // Listening events on body  of the document
+        document.body.addEventListener("keydown", handleKeyDown);
+        return () =>
+            document.body.removeEventListener("keydown", handleKeyDown);
+    }, []);
     return (
         <StyledWrapper>
-            <Button onClick={() => handleKeyDown()}>MR</Button>
-            <Button onClick={() => handleKeyDown()}>MC</Button>
-            <Button onClick={() => handleKeyDown()}>M+</Button>
-            <Button onClick={() => handleKeyDown()}>M-</Button>
-            <Button onClick={() => handleKeyDown()} color="red">
+            <Button onClick={onMemoryRecallButtonClick}>MR</Button>
+            <Button onClick={onMemoryClearButtonClick}>MC</Button>
+            <Button onClick={onMemoryClearButtonClick}>M+</Button>
+            <Button onClick={onMemoryClearButtonClick}>M-</Button>
+            <Button onClick={onMemoryClearButtonClick} color="red">
                 AC
             </Button>
-            <Button onClick={() => handleKeyDown()}>C</Button>
-            <Button onClick={() => handleKeyDown()}>-/+</Button>
-            <Button onClick={() => handleKeyDown()} color="dark">
+            <Button onClick={onMemoryClearButtonClick}>C</Button>
+            <Button onClick={onMemoryClearButtonClick}>-/+</Button>
+            <Button onClick={onMemoryClearButtonClick} color="dark">
                 ÷
             </Button>
-            <Button onClick={() => handleKeyDown()}>7</Button>
-            <Button onClick={() => handleKeyDown()}>8</Button>
-            <Button onClick={() => handleKeyDown()}>9</Button>
-            <Button onClick={() => handleKeyDown()} color="dark">
+            <Button onClick={onMemoryClearButtonClick}>7</Button>
+            <Button onClick={onMemoryClearButtonClick}>8</Button>
+            <Button onClick={onMemoryClearButtonClick}>9</Button>
+            <Button onClick={onMemoryClearButtonClick} color="dark">
                 ×
             </Button>
-            <Button onClick={() => handleKeyDown()}>4</Button>
-            <Button onClick={() => handleKeyDown()}>5</Button>
-            <Button onClick={() => handleKeyDown()}>6</Button>
-            <Button onClick={() => handleKeyDown()} color="dark">
+            <Button onClick={onMemoryClearButtonClick}>4</Button>
+            <Button onClick={onMemoryClearButtonClick}>5</Button>
+            <Button onClick={onMemoryClearButtonClick}>6</Button>
+            <Button onClick={onMemoryClearButtonClick} color="dark">
                 -
             </Button>
-            <Button onClick={() => handleKeyDown()}>1</Button>
-            <Button onClick={() => handleKeyDown()}>2</Button>
-            <Button onClick={() => handleKeyDown()}>3</Button>
-            <Button onClick={() => handleKeyDown()} color="dark">
+            <Button onClick={onMemoryClearButtonClick}>1</Button>
+            <Button onClick={onMemoryClearButtonClick}>2</Button>
+            <Button onClick={onMemoryClearButtonClick}>3</Button>
+            <Button onClick={onMemoryClearButtonClick} color="dark">
                 +
             </Button>
-            <Button onClick={() => handleKeyDown()}>0</Button>
-            <Button onClick={() => handleKeyDown()}>.</Button>
+            <Button onClick={onMemoryClearButtonClick}>0</Button>
+            <Button onClick={onMemoryClearButtonClick}>.</Button>
             <Button
-                onClick={() => handleKeyDown()}
+                onClick={onMemoryClearButtonClick}
                 color="green"
                 isLarge={true}
             >
